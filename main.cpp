2dd25ec9e35ffb796f391f1fd19c6388d0f1060d
carlonavone <cnavone@mail.csuchico.edu>
2020-02-12 09:56:34 -0800

    macdonal

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+
+int main()
+{
+    cout<<"Hi, please enter two numbers: ";
+    int num1;
+    int num2;
+    cin >> num1;
+    cin >> num2;
+    return 0;
+}
